fd3002174bc5c5933deb6a871c86a39613288544
Jhuls123 <jpunding05@gmail.com>
2022-02-13 11:10:46 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Juls Heroku Deploy Testing...</p>
+				<p>Juls Netlify Deploy Testing...</p>
 				<a
 					className='App-link'
 					href='https://reactjs.org'
